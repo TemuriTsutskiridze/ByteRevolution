25e3c31523174aedc4efcc0d3347ecdd0038fd37
Lasha Gurgenidze <lashagurgenidze1989@gmail.com>
2024-01-16 09:21:35 -0700

    create and display Header component, add some temporary assets

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <Head>
+      <img src="/assets/Logo.png" alt="Logo" />
+
+      <Button>შესვლა</Button>
+    </Head>
+  );
+}
+
+const Head = styled.header`
+  padding: 2rem 7.6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #e4e3eb;
+`;
+
+const Button = styled.button`
+  background: #5d37f3;
+  border: none;
+  border-radius: 0.8rem;
+  padding: 1rem 2rem;
+  color: white;
+  font-size: 1.4rem;
+  font-weight: 500;
+  line-height: 2rem;
+`;
