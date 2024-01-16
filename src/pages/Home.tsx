48d5b34e9514261bfe659842fba818c4a140550c
Lasha Gurgenidze <lashagurgenidze1989@gmail.com>
2024-01-16 09:28:17 -0700

    in home component add top section: h1 and image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,34 @@
+import styled from "styled-components";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Main>
+      <Title>
+        <h1>ბლოგი</h1>
+        <img src="/assets/Blog.png" alt="Blog" />
+      </Title>
+    </Main>
+  );
 }
+
+const Main = styled.div`
+  padding: 6.4rem 7.6rem 9.6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  h1 {
+    font-size: 6.4rem;
+    font-weight: 700;
+    line-height: 7.2rem;
+    color: #1a1a1f;
+    padding-left: 1.3rem;
+  }
+`;
