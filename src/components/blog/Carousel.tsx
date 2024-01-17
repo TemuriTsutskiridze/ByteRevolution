ba5298f609f5c838bd2ec1173333bf91fc7243f7
Lasha Gurgenidze <lashagurgenidze1989@gmail.com>
2024-01-17 12:11:28 -0700

    add style for carousel arrows

--- src/components/blog/Carousel.tsx
+++ src/components/blog/Carousel.tsx
@@ -9,87 +9,143 @@ export default function Carousel() {
 
   return (
     <>
-      <BlogContainer>
-        {blogData.map((item, index) => {
-          if (start <= index && end >= index) {
-            return (
-              <Blog key={item.id}>
-                <img src={item.image} alt="image" />
-                <Name>{item.author}</Name>
-                <Date>{item.publish_date}</Date>
-                <Title>{item.title}</Title>
-                <Categories>
-                  {item.categories.map((category) => (
-                    <ListItem
-                      key={category.name}
-                      name={category.name}
-                      color={category.color}
-                      bgcolor={category.bgColor}
-                    >
-                      {category.name}
-                    </ListItem>
-                  ))}
-                </Categories>
-                <Description>{`${item.description.slice(
-                  0,
-                  86
-                )}...`}</Description>
-                <Link to={`/${item.id}`}>
-                  <ItselfLink>
-                    <p>სრულად ნახვა</p>
-                    <img src="/assets/Arrow.svg" />
-                  </ItselfLink>
-                </Link>
-              </Blog>
-            );
-          }
-        })}
-      </BlogContainer>
+      <CarouselContainer>
+        <SimilarBlogsDiv>
+          <SimilarBlogs>მსგავსი სტატიები</SimilarBlogs>
+          <ButtonsDiv>
+            <Prev
+              onClick={() => {
+                if (start > 0) {
+                  setStart((prev) => --prev);
+                  setEnd((next) => --next);
+                }
+              }}
+            >
+              {">"}
+            </Prev>
+            <Next
+              onClick={() => {
+                if (end < blogData.length - 1) {
+                  setEnd((next) => ++next);
+                  setStart((prev) => ++prev);
+                }
+              }}
+            >
+              {">"}
+            </Next>
 
-      <Prev
-        onClick={() => {
-          if (start > 0) {
-            setStart((prev) => --prev);
-            setEnd((next) => --next);
-          }
-        }}
-      >
-        {"<"}
-      </Prev>
-      <Next
-        onClick={() => {
-          if (end < blogData.length - 1) {
-            setEnd((next) => ++next);
-            setStart((prev) => ++prev);
-          }
-        }}
-      >
-        {">"}
-      </Next>
+            <ArrowLeft src="/assets/ArrowL.svg" alt="left" />
+            <ArrowRight src="/assets/ArrowR.svg" alt="right" />
+          </ButtonsDiv>
+        </SimilarBlogsDiv>
+
+        <BlogContainer>
+          {blogData.map((item, index) => {
+            if (start <= index && end >= index) {
+              return (
+                <Blog key={item.id}>
+                  <img src={item.image} alt="image" />
+                  <Name>{item.author}</Name>
+                  <Date>{item.publish_date}</Date>
+                  <Title>{item.title}</Title>
+                  <Categories>
+                    {item.categories.map((category) => (
+                      <ListItem
+                        key={category.name}
+                        name={category.name}
+                        color={category.color}
+                        bgcolor={category.bgColor}
+                      >
+                        {category.name}
+                      </ListItem>
+                    ))}
+                  </Categories>
+                  <Description>{`${item.description.slice(
+                    0,
+                    86
+                  )}...`}</Description>
+                  <Link to={`/${item.id}`}>
+                    <ItselfLink>
+                      <p>სრულად ნახვა</p>
+                      <img src="/assets/Arrow.svg" />
+                    </ItselfLink>
+                  </Link>
+                </Blog>
+              );
+            }
+          })}
+        </BlogContainer>
+      </CarouselContainer>
     </>
   );
 }
 
+const CarouselContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 9.8rem;
+`;
+
+const SimilarBlogsDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const SimilarBlogs = styled.p`
+  color: #1a1a1f;
+  font-size: 3.2rem;
+  font-weight: 700;
+  line-height: 4rem;
+`;
+
+const ButtonsDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 11.2rem;
+  position: relative;
+`;
+
 const Prev = styled.button`
-  width: 40px;
-  height: 20px;
+  width: 4.5rem;
+  height: 4.5rem;
+  border: none;
+  border-radius: 50%;
+  opacity: 0;
+  z-index: 1;
+  cursor: pointer;
 `;
 
 const Next = styled.button`
-  width: 40px;
-  height: 20px;
+  width: 4.5rem;
+  height: 4.5rem;
+  border: none;
+  border-radius: 50%;
+  opacity: 0;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const ArrowLeft = styled.img`
+  position: absolute;
+`;
+const ArrowRight = styled.img`
+  position: absolute;
+  left: 6.8rem;
 `;
 
 const BlogContainer = styled.div`
   display: flex;
   gap: 3.2rem;
-  margin-top: 6.4rem;
+  margin-top: 4rem;
 `;
 
 const Blog = styled.div`
   display: flex;
   flex-direction: column;
   width: 40.2rem;
+  transition: 500ms opacity ease-in-out;
 `;
 
 const Name = styled.p`
