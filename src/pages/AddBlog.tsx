4ac97df834c6f1c9c4c64fef62c8149535bff3ce
MariamKvirchiladze <kvircho555@gmail.com>
2024-01-17 16:46:18 +0400

    refurbish: style: make some changes in the structure, and add some styles

--- src/pages/AddBlog.tsx
+++ src/pages/AddBlog.tsx
@@ -1,58 +1,109 @@
 import img from "/public/folder-add.png";
+import styled from "styled-components";
 
 export default function AddBlog() {
   return (
-    <form>
-      <div>
-        <h1>ბლოგის დამატება</h1>
+    <Container>
+      <BlogForm>
         <div>
-          <h3>ატვირთეთ ფოტო</h3>
+          <h1>ბლოგის დამატება</h1>
           <div>
-            <img src={img} alt="img" />
-            <p>
-              ჩააგდეთ ფაილი აქ ან <span>აირჩიეთ ფაილი</span>
+            <h3>ატვირთეთ ფოტო</h3>
+            <div className="addPhoto">
               <input type="file" />
-            </p>
+              <img src={img} alt="img" />
+              <p>
+                ჩააგდეთ ფაილი აქ ან <span>აირჩიეთ ფაილი</span>
+              </p>
+            </div>
+          </div>
+          <div>
+            <section className="author&title">
+              <div className="author">
+                <h3>ავტორი *</h3>
+                <input type="text" placeholder="შეიყვანეთ ავტორი" />
+                <ul>
+                  <li>მინიმუმ 4 სიმბოლო </li>
+                  <li>მინიმუმ ორი სიტყვა</li>
+                  <li>მხოლოდ ქართული სიმბოლოები</li>
+                </ul>
+              </div>
+              <div className="title">
+                <h3>სათაური *</h3>
+                <input type="text" placeholder="შეიყვანეთ სათაური" />
+                <ul>
+                  <li>მინიმუმ 2 სიმბოლო </li>
+                </ul>
+              </div>
+            </section>
+            <section className="describe">
+              <h3>აღწერა *</h3>
+              <input type="text" placeholder="შეიყვანეთ აღწერა" />
+              <ul>
+                <li>მინიმუმ 2 სიმბოლო </li>
+              </ul>
+            </section>
+            <section className="date&category&mail">
+              <div className="date">
+                <h3>გამოქვეყნების თარიღი *</h3>
+                <input type="date" />
+              </div>
+              <div className="category">
+                <h3>კატეგორია *</h3>
+                <input type="text" placeholder="აირჩიეთ კატეგორია" />
+              </div>
+              <div className="mail">
+                <h3>ელ-ფოსტა *</h3>
+                <input type="text" placeholder="Example@redberry.ge" />
+              </div>
+            </section>
           </div>
         </div>
-        <div>
-          <div>
-            <h3>ავტორი *</h3>
-            <input type="text" placeholder="შეიყვანეთ ავტორი" />
-            <ul>
-              <li>მინიმუმ 4 სიმბოლო </li>
-              <li>მინიმუმ ორი სიტყვა</li>
-              <li>მხოლოდ ქართული სიმბოლოები</li>
-            </ul>
-          </div>
-          <div>
-            <h3>სათაური *</h3>
-            <input type="text" placeholder="შეიყვანეთ სათაური" />
-            <ul>
-              <li>მინიმუმ 2 სიმბოლო </li>
-            </ul>
-          </div>
-          <div>
-            <h3>აღწერა *</h3>
-            <input type="text" placeholder="შეიყვანეთ აღწერა" />
-            <ul>
-              <li>მინიმუმ 2 სიმბოლო </li>
-            </ul>
-          </div>
-          <div>
-            <h3>გამოქვეყნების თარიღი *</h3>
-            <input type="date" />
-          </div>
-          <div>
-            <h3>კატეგორია *</h3>
-            <input type="text" placeholder="აირჩიეთ კატეგორია" />
-          </div>
-          <div>
-            <h3>ელ-ფოსტა *</h3>
-            <input type="text" placeholder="Example@redberry.ge" />
-          </div>
-        </div>
-      </div>
-    </form>
+      </BlogForm>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const BlogForm = styled.form`
+  width: 600px;
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 40px;
+    margin: 40px 0;
+  }
+  h3 {
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px;
+  }
+  .addPhoto {
+    display: flex;
+    width: 100%;
+    height: 180px;
+    border-radius: 12px;
+    border: 1px dashed #85858d;
+    background: #f4f3ff;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    img {
+      margin-bottom: 24px;
+    }
+    input {
+      opacity: 0.2;
+      position: relative;
+      z-index: 1;
+    }
+  }
+`;
